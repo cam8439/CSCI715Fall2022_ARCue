23c8b012dd3c0ee5bb88975d6a374739fca9ee86
Claire :) <cmurray@sandboxunion.com>
2022-12-13 21:08:18 -0500

    Music Sync Code

---
+++ MacSynchro/SynchroTest/Program.cs
@@ -0,0 +1,24 @@
+﻿//USE THIS VERSION FOR MAC
+using System.Net;
+using System.Diagnostics;
+
+using (var client = new WebClient())
+{
+    var s = client.DownloadString(@"https://www.cs.rit.edu/~cam8439/syncTime.txt");
+    if (s == "-1"){
+        Console.WriteLine("no sync time yet.");
+        return;
+    }
+    Console.WriteLine(s);
+    var J = DateTimeOffset.FromUnixTimeMilliseconds(long.Parse(s));
+    var K = DateTimeOffset.Now;
+    Console.WriteLine(J.TimeOfDay);
+    Console.WriteLine(K.TimeOfDay);
+    //Console.WriteLine(J-K);
+    while(K.TimeOfDay < J.TimeOfDay)
+    {
+        K = DateTimeOffset.Now;
+    }
+    Console.WriteLine(J - DateTime.Now);
+    Process.Start( new ProcessStartInfo { FileName = @"/Users/claire/Desktop/SynchroTest/cmd.exe", UseShellExecute = true } );
+}
---
+++ WindowsSynchro/SynchroTest/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+// USE THIS VERSION FOR WINDOWS
+using System.Media;
+using System.Net;
+
+using (var client = new WebClient())
+{
+    //Make sure that the sound file is updated and accurate
+    SoundPlayer sound = new SoundPlayer("Singring.wav");
+
+    var s = client.DownloadString(@"https://www.cs.rit.edu/~cam8439/syncTime.txt");
+Console.WriteLine(s);
+    var J = DateTimeOffset.FromUnixTimeMilliseconds(long.Parse(s));
+    var K = DateTimeOffset.Now;
+    Console.WriteLine(J.TimeOfDay);
+    Console.WriteLine(K.TimeOfDay);
+    while (K.TimeOfDay < J.TimeOfDay)
+    {
+        K = DateTimeOffset.Now;
+    }
+    Console.WriteLine(J - DateTime.Now);
+    sound.PlaySync();
+}
